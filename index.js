244a2da99b2360328ab124b4289a824ad0cbc082
Jiacheng-coder <2253541907@qq.com>
2023-07-16 20:53:23 +0800

    fix

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-import { count } from "./counter";
-import _ from 'lodash-es' 
+import { count } from './counter'
+import _ from 'lodash-es'
 
 console.log(count)
-console.log("lodash", _)
+console.log('lodash', _)
+console.log('import.meta.env\n', import.meta.env)
+const BASE_URL = import.meta.env.MEI_TUAN_BASE_URL
+console.log('BASE_URL:\n', BASE_URL)
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,23 @@
-export default {
-  optimizeDeps: {
-    // exclude: ["lodash-es"]
-  }
+import { defineConfig } from 'vite'
+import viteBaseConfig from './vite.base.config'
+import viteDevConfig from './vite.dev.config'
+import viteProdConfig from './vite.prod.config'
+import { loadEnv } from 'vite'
+
+const envResolver = {
+  build: () => {
+    console.log('生产环境')
+    return { ...viteBaseConfig, ...viteProdConfig }
+  },
+  serve: () => {
+    console.log('开发环境')
+    return { ...viteBaseConfig, ...viteDevConfig }
+  },
 }
+
+export default defineConfig(({ command, mode }) => {
+  console.log('mode\n', mode)
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('env.BASE_URL:\n', env.MEI_TUAN_BASE_URL)
+  return envResolver[command]()
+})
